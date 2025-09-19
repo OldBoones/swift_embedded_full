e2035ef6437adf9bb310496033844cb733732e06
Heiko Ritter <heiko.ritter@icloud.com>
2025-09-20 00:53:51 +0200

    v0.1 working logging functionality

--- main/Main.swift
+++ main/Main.swift
@@ -12,21 +12,26 @@
 // The code will blink an LED on GPIO8. To change the pin, modify Led(gpioPin: 8)
 @_cdecl("app_main")
 func main() {
+  let blinkDelayMs: UInt32 = 500
+  var high15 = false
   print("Hello from Swift on ESP32-C6!")
 
   let pin15 = GPIO(pin: 15, direction: .output)
-  pin15.write(true)
-  log("where is my log?", .debug)
-
-  var ledValue: Bool = false
-  let blinkDelayMs: UInt32 = 500
-  let led = Led(gpioPin: 8)
-
+  pin15.write(high15)
+  vTaskDelay(blinkDelayMs * 10 / (1000 / UInt32(configTICK_RATE_HZ)))
+  log("where is my log?", .error)
+  //var ledValue: Bool = false
+  //let led = Led(gpioPin: 8)
+  pin15.write(!high15)
+  vTaskDelay(blinkDelayMs * 10 / (1000 / UInt32(configTICK_RATE_HZ)))
+  log("starting main loop", .info)
   while true {
-    pin15.write(!pin15.read())
-    led.setLed(value: ledValue)
-    ledValue.toggle()  // Toggle the boolean value
-    vTaskDelay(blinkDelayMs / (1000 / UInt32(configTICK_RATE_HZ)))
+    high15.toggle()
+    pin15.write(high15)
+    log ("Toggling pin 15 to \(high15)", .warn)
+    //led.setLed(value: ledValue)
+    //ledValue.toggle()  // Toggle the boolean value
+    vTaskDelay(blinkDelayMs / (500 / UInt32(configTICK_RATE_HZ)))
     //log("Nee echt, wieder eine Schleife durchlaufen", level: .debug)
   }
 }
